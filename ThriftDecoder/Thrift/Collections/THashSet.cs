19042ff7b6fa8e1eb54aff8ffde646b685a70a0f
Rob <rob@nulldev.uk>
2019-02-28 19:12:23 +0000

    Create HashSets

---
+++ ThriftDecoder/Thrift/Collections/THashSet.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace ThriftDecoder.Thrift.Collections
+{
+    public class THashSet<T> : IEnumerable, IEnumerable<T>, ICollection<T>
+    {
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            return ((IEnumerable<T>)this.set).GetEnumerator();
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.set.Count;
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public void Add(T item)
+        {
+            this.set.Add(item);
+        }
+
+        public void Clear()
+        {
+            this.set.Clear();
+        }
+
+        public bool Contains(T item)
+        {
+            return this.set.Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            this.set.CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return this.set.GetEnumerator();
+        }
+
+        public bool Remove(T item)
+        {
+            return this.set.Remove(item);
+        }
+
+        private HashSet<T> set = new HashSet<T>();
+    }
+}
